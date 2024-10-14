abdd2caaf56c5cfad8d9e5cdd8c9ae63d7596cf0
Joao Costa <jpmoreiracosta10@gmail.com>
2024-10-14 22:34:42 +0100

    Added Participants where is was null

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import ast
 
 def getSmallDescriptions():
     df = pd.read_csv("final.csv")
@@ -12,4 +13,34 @@ def getSmallDescriptions():
     
     return len(small_descriptions)
 
-print(getSmallDescriptions())
+#print(getSmallDescriptions())
+
+def cleanNullParticipants():
+    df = pd.read_csv("final.csv")
+
+    dfFiltered = df[(df['Winner'] == 'Massacre') | (df['Winner'] == 'Draw') | (df['Winner'] == 'Accidental incident') | (df['Winner'] == 'Incident')]
+    dfFiltered.loc[dfFiltered['Participants'].isnull(), 'Participants'] = "[]"
+
+    df.update(dfFiltered)
+    print(df[df['Participants'].isnull()]['ID'])
+
+    """
+    df.loc[df['Participants'].isnull(), 'Participants'] = df.apply(
+        lambda row: [row['Winner'], row['Loser']] if pd.notnull(row['Winner']) and pd.notnull(row['Loser']) else [], axis=1
+    )
+    """
+
+    df.loc[df['Participants'].isnull(), 'Participants'] = df.apply(
+        lambda row: (
+            [name.strip() for name in str(row['Winner']).split(',')] + 
+            [name.strip() for name in str(row['Loser']).split(',')]
+        ) if pd.notnull(row['Winner']) and pd.notnull(row['Loser']) else [], 
+        axis=1
+    )
+
+    #print(df['Participants'].isnull().sum())
+
+
+    df.to_csv("final.csv", index=False)
+
+cleanNullParticipants()
