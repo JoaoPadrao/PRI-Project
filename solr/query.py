f3f5cc6d211b900b1f84699cf3a98c9cb43af060
Joao Costa <jpmoreiracosta10@gmail.com>
2024-12-17 22:24:23 +0000

    Still not working

--- solr/query.py
+++ solr/query.py
@@ -31,6 +31,13 @@ def query_solr():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+# Add CORS headers to all responses
+# @app.after_request
+# def add_cors_headers(response):
+#     response.headers["Access-Control-Allow-Origin"] = "*"
+#     response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+#     response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+#     return response
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000)
